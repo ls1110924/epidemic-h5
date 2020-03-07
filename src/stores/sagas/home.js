f17e21aca760841ec5bfcd5991d21421c1c0df30
A Shuai <lishuai28@meituan.com>
2020-03-08 03:35:27 +0800

    feat: 优化提交逻辑

--- src/stores/sagas/home.js
+++ src/stores/sagas/home.js
@@ -1,7 +1,9 @@
-import { all, call, put, select, takeLatest } from 'redux-saga/effects'
+import { all, put, select, takeLatest } from 'redux-saga/effects'
+import { message } from 'antd'
 import { HOME_ACTIONS } from '../actions'
+import { isEmptyStr, PHONE_REG, ID_NUM_REG, TEMPERATURE_REG } from '../../utils'
 
-function* handleSubmit(action) {
+function* handleSubmit() {
     try {
         const state = yield select()
         const home = state.home
@@ -10,14 +12,50 @@ function* handleSubmit(action) {
             return
         }
 
-        // 校验参数
-
         yield put(HOME_ACTIONS.toggleSubmitting(true))
 
-        // TODO
+        // 校验参数
+        const name = home.get('name')
+        const idNum = home.get('idNum')
+        const phone = home.get('phone')
+        const area = home.get('area')
+        const temperature = home.get('temperature')
+
+        if (isEmptyStr(name)) {
+            throw new Error('姓名不得为空')
+        }
+        if (isEmptyStr(idNum)) {
+            throw new Error('身份证号不得为空')
+        }
+        if (!(ID_NUM_REG.test(idNum))) {
+            throw new Error('请输入正确的身份证号')
+        }
+        if (isEmptyStr(phone)) {
+            throw new Error('手机号不得为空')
+        }
+        if (!(PHONE_REG.test(phone))) {
+            throw new Error('请输入正确的手机号')
+        }
+        if (isEmptyStr(area)) {
+            throw new Error('请选择当前所在地区')
+        }
+        if (isEmptyStr(temperature)) {
+            throw new Error('体温不得为空')
+        }
+        if (!TEMPERATURE_REG.test(temperature)) {
+            throw new Error('请输入正确的体温数字')
+        }
+
+        // TODO 网络请求
+
+
+        message.info('提交成功')
         yield put(HOME_ACTIONS.clear())
         yield put(HOME_ACTIONS.toggleSubmitting(false))
     } catch (e) {
+        if (e.message) {
+            message.info(e.message)
+        }
         yield put(HOME_ACTIONS.toggleSubmitting(false))
     }
 }
---
+++ src/utils/reg.js
@@ -0,0 +1,6 @@
+// 身份证号校验
+export const ID_NUM_REG = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/
+// 手机号校验
+export const PHONE_REG = /^1[3456789]\d{9}$/
+// 体温校验
+export const TEMPERATURE_REG = /^[1-9]\d(\.\d+)?$/
