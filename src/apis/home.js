8445e458f58c279da818078e517cff6567f2832e
A Shuai <lishuai28@meituan.com>
2020-03-08 18:58:17 +0800

    feat: 增加注释

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,12 +1,18 @@
-import axios from './axios';
+import axios from './axios'
 
+/**
+ * 提交健康信息
+ * @param form
+ * @returns {Promise<*>}
+ */
 export async function submit(form) {
     // return new Promise(resolve => {
     //     setTimeout(() => {
     //         resolve(MOCK_RESULT)
     //     }, 2000)
     // })
-    return axios.post('/proxy/api/epidemic/submit', form )
+
+    return axios.post('/proxy/api/epidemic/submit', form)
 }
 
 const MOCK_RESULT = {
